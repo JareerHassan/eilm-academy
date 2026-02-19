7114925763c48b77445825f56795101308f7b432
Jareer Hassan <jareerhassan8@gmail.com>
2026-02-19 15:38:37 +0500

    add

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -17,7 +17,7 @@ import InstructorOne from "@/components/InstructorOne";
 export const metadata = {
   title: "E-ILM-ACADEMY",
   description:
-    "EduAll is a comprehensive and modern NEXT JS template designed for online education platforms, learning management systems (LMS), tutors, educational institutions, and online courses. It’s the perfect solution for creating an engaging and interactive online learning experience for students, educators, and institutions. Whether you’re offering online courses, running a tutoring platform, or managing an educational website, EduAll provides the tools to help you succeed. This template is tailored to meet the needs of educators, administrators, and students, providing a seamless and engaging user experience.",
+  "Eilm Academy is a global online Islamic learning platform that provides authentic and structured education in the Qur'an, Hadith, Hanafi Fiqh, Arabic, and Tajweed. Gain knowledge from qualified scholars through live and recorded classes from the comfort of your home."
 };
 
 const page = () => {
